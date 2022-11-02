1f8cb737948b4316b40622bc8581f9cd8e6aafd9
LssLn <lssln97@gmail.com>
2022-11-02 19:41:31 +0100

    004

--- r004-isbn-verifier/src/main.rs
+++ r004-isbn-verifier/src/main.rs
@@ -7,7 +7,25 @@ fn main() -> io::Result<()>{ //io::Result is used to
     //reads the input and stores the value in the isbn variable
     stdin.read_line(&mut isbn)?;
     println!("input {} ", isbn);
+    isbn= isbn.trim().parse().unwrap(); // trimming newline character
 
+    //iterating each char of the string isbn
+    let mut count =10;
+    println!("isbn has {} digits",count);
+
+    let mut sum;
+    for c in isbn.chars() {
+        let mut c_number = c as i32 - 0x30;
+        if c != '-' {
+            if c == 'X' {
+                println!("X is 10");
+                c_number = 10;
+            }
+            sum = c_number * (count as i32);
+            println!("c={}, c_number={}  |  count={}  | sum={}", c, c_number, count as i32, sum);
+            count = count -1;
+        }
+    }
 
 
 
